0103ed30546b5255dd1a8679af05a85423b90f3e
mjlynch123 <mjlynch35@yahoo.com>
2024-02-20 17:53:40 -0600

    changed route

--- controllers/api/api-routes.js
+++ controllers/api/api-routes.js
@@ -1,7 +1,4 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-    res.json({ message: "Hello, World!" });
-});
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
 const Message = require("../models/Message");
 
+router.get("/", (req, res) => {
+    res.json({ message: "Hello, World!" });
+});
+
 module.exports = router;
